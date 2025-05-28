cff792066835e407ada55be8e1ae55a08c6d6e09
Manuel <manuel.turetta@pm.me>
2025-05-28 20:34:55 +0200

    refactor: improve cosmos find events/attribute logic

--- cosmos/types/abci.go
+++ cosmos/types/abci.go
@@ -1,79 +1,93 @@
 package types
 
-type ABCIEvent struct {
-	Type       string               `json:"type"`
-	Attributes []ABCIEventAttribute `json:"attributes"`
-}
+import (
+	"slices"
 
-func (e *ABCIEvent) FindAttribute(key string) (ABCIEventAttribute, bool) {
-	for _, attribute := range e.Attributes {
-		if attribute.Key == key {
-			return attribute, true
-		}
-	}
-
-	return ABCIEventAttribute{}, false
-}
-
-func (e *ABCIEvent) FindAttributes(key string) []ABCIEventAttribute {
-	var results []ABCIEventAttribute
-	for _, attribute := range e.Attributes {
-		if attribute.Key == key {
-			results = append(results, attribute)
-		}
-	}
-
-	return results
-}
-
-func (e *ABCIEvent) FindAttributeFunc(predicate func(a ABCIEventAttribute) bool) (ABCIEventAttribute, bool) {
-	for _, attribute := range e.Attributes {
-		if predicate(attribute) {
-			return attribute, true
-		}
-	}
-
-	return ABCIEventAttribute{}, false
-}
-
-func (e *ABCIEvent) FindAttributesFunc(predicate func(a ABCIEventAttribute) bool) []ABCIEventAttribute {
-	var results []ABCIEventAttribute
-	for _, attribute := range e.Attributes {
-		if predicate(attribute) {
-			results = append(results, attribute)
-		}
-	}
-
-	return results
-}
+	"github.com/milkyway-labs/chain-indexer/utils"
+)
 
 type ABCIEventAttribute struct {
 	Key   string `json:"key"`
 	Value string `json:"value"`
 }
 
+// ---------------------------------------------------------------------------
+// ---- ABCI Event
+// ---------------------------------------------------------------------------
+
+type ABCIEvent struct {
+	Type       string               `json:"type"`
+	Attributes []ABCIEventAttribute `json:"attributes"`
+}
+
+// FindAttribute finds the first attribute that matches the given predicate
+func (e *ABCIEvent) FindAttributeFunc(predicate func(a ABCIEventAttribute) bool) (ABCIEventAttribute, bool) {
+	index := slices.IndexFunc(e.Attributes, predicate)
+	if index == -1 {
+		return ABCIEventAttribute{}, false
+	}
+
+	return e.Attributes[index], true
+}
+
+// FindAttribute finds the first attribute with the given key
+func (e *ABCIEvent) FindAttribute(key string) (ABCIEventAttribute, bool) {
+	return e.FindAttributeFunc(func(a ABCIEventAttribute) bool {
+		return a.Key == key
+	})
+}
+
+// FindAttributesFunc finds all the attributes that match the given predicate
+func (e *ABCIEvent) FindAttributesFunc(predicate func(a ABCIEventAttribute) bool) []ABCIEventAttribute {
+	return utils.Filter(e.Attributes, predicate)
+}
+
+// FindAttributes finds all the attributes with the given key
+func (e *ABCIEvent) FindAttributes(key string) []ABCIEventAttribute {
+	return e.FindAttributesFunc(func(a ABCIEventAttribute) bool {
+		return a.Key == key
+	})
+}
+
+// ---------------------------------------------------------------------------
+// ---- ABCI Event slice
+// ---------------------------------------------------------------------------
+
 type ABCIEvents []ABCIEvent
 
-func (events *ABCIEvents) GetEventWitType(t string) (ABCIEvent, bool) {
-	for _, e := range *events {
-		if e.Type == t {
-			return e, true
-		}
+// FindEventFunc finds the first event that matches the given predicate
+func (events ABCIEvents) FindEventFunc(predicate func(e ABCIEvent) bool) (ABCIEvent, bool) {
+	index := slices.IndexFunc(events, predicate)
+
+	if index == -1 {
+		return ABCIEvent{}, false
 	}
 
-	return ABCIEvent{}, false
+	return events[index], true
 }
 
-func (events *ABCIEvents) GetEventsWithType(t string) []ABCIEvent {
-	var results []ABCIEvent
-	for _, e := range *events {
-		if e.Type == t {
-			results = append(results, e)
-		}
-	}
-
-	return results
+// FindEventWithType finds the first event with the given type
+func (events ABCIEvents) FindEventWithType(t string) (ABCIEvent, bool) {
+	return events.FindEventFunc(func(e ABCIEvent) bool {
+		return e.Type == t
+	})
 }
+
+// FindEventsFunc finds all the events that match the given predicate
+func (events ABCIEvents) FindEventsFunc(predicate func(e ABCIEvent) bool) []ABCIEvent {
+	return utils.Filter(events, predicate)
+}
+
+// FindEventsWithType finds all the events with the given type
+func (events ABCIEvents) FindEventsWithType(t string) []ABCIEvent {
+	return events.FindEventsFunc(func(e ABCIEvent) bool {
+		return e.Type == t
+	})
+}
+
+// ---------------------------------------------------------------------------
+// ---- ABCI Event contained inside the tx log
+// ---------------------------------------------------------------------------
 
 type ABCIMsgLog struct {
 	MsgIndex uint32     `json:"msg_index"`
---
+++ utils/slice.go
@@ -0,0 +1,12 @@
+package utils
+
+// Returns the elements of `slice` that satisfy `predicate`.
+func Filter[T any](slice []T, predicate func(T) bool) []T {
+	var result []T
+	for _, v := range slice {
+		if predicate(v) {
+			result = append(result, v)
+		}
+	}
+	return result
+}
